b9d2af53262c9066247b531e275141375b99f6c5
Qume <qume2005@outlook.com>
2025-03-05 14:45:10 +0800

    Update serial.ts

--- src/stores/serial.ts
+++ src/stores/serial.ts
@@ -1,5 +1,14 @@
 import { defineStore } from 'pinia'
+import { ref } from 'vue';
 
-export const useSerialStore = defineStore('serial', {
+function set_serialport(name: string): {
 
+}
+
+export const useSerialStore = defineStore('serial', () => {
+    let serialName = ref<string | undefined>(undefined);
+    let dataSeq = ref<number | undefined>(undefined);
+    let serialports = ref<string | undefined>(undefined);
+    let
+    return;
 })
