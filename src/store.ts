d5299810f92972b882c392e320aa33a0ea479410
Qume <qume2005@outlook.com>
2025-03-05 17:48:35 +0800

    Finished more works

--- src/store.ts
+++ src/store.ts
@@ -2,7 +2,10 @@ import { ref } from "vue";
 import { getSerialports, setSerialport, getDataSeq } from "./serial";
 import { defineStore } from "pinia";
 
-export const useAppStore = defineStore("app", ()=>{
+export const useAppStore = defineStore("app", () => {
     const selectedPort = ref<string>();
     const dataSeq = ref<[number]>();
+    const msg = ref<string>();
+    const serialports = ref<[number]>();
+
 })
