5475d9a1240f7e5a2ceeab8ae98c37e3a8d10c22
Qume <qume2005@outlook.com>
2025-03-10 01:22:16 +0800

    Update serial.ts

--- src/stores/serial.ts
+++ src/stores/serial.ts
@@ -3,6 +3,10 @@ import { commandGetSerialports, commandConnectPort, commandGetDataSeq, commandRe
 import { defineStore } from 'pinia';
 
 enum SerialState {
+    Disconnected,
+    Connecting,
+    Connected,
+    Retry,
 
 }
 
@@ -10,7 +14,7 @@ export const useAppStore = defineStore('app', () => {
     const dataSeq = ref<number[]>([]);
     const serialports = ref<string[]>([]);
     const port = ref<string | null>(null);
-    const serialState = ref<SerialState>();
+    const serialState = ref<SerialState>(SerialState.Disconnected);
 
     async function updateSerialports(): Promise<void> {
         serialports.value = await commandGetSerialports();
@@ -27,11 +31,13 @@ export const useAppStore = defineStore('app', () => {
 
     async function resetSerialport(): Promise<void> {
         await commandResetPort();
+        port.value = null;
     };
 
     async function connectPort(name: string): Promise<void | Error> {
         try {
             await commandConnectPort(name);
+            port.value = name;
         } catch (err) {
             return new Error(`${err}`);
         }
